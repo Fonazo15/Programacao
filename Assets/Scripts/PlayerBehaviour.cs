c619c9ff54bf7a3c43fc97aaf0beb2cecee6042f
Fonazo15 <felipeantoniodesouzaesilva@gmail.com>
2025-02-13 15:09:10 -0300

    Terminado Desafio 1

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -8,33 +8,39 @@ public class PlayerBehaviour : MonoBehaviour
     public UnityEvent Reset;
     public Transform player;
 
-    private Vector2 playerStartPos;
-    public Vector2 playerCurrentPos;
+    public Vector3 playerPos;
 
-    public float fallSpd = 0.01f;
+    public float flySpd = 0.01f;
+    private float _newHeight;
     void Start()
     {
         player = GetComponent<Transform>();
-        playerStartPos = player.position;
     }
     void Update()
     {
-        Fall();
+        Jump();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Floor") || collision.gameObject.CompareTag("Pipe"))
         {
-            Reset.Invoke();
+            //Reset.Invoke();
         }
-    }
-
-    void Fall()
-    {
-
     }
     void Jump()
     {
 
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                _newHeight = player.position.y + flySpd;
+
+                playerPos = new Vector3(player.position.x, _newHeight, 0);
+
+                player.position = Vector3.Lerp(player.position, playerPos, flySpd * Time.deltaTime);
+            }
+        }
     }
 }
