085e5de201be2975d179f20fee49065df16303de
Fonazo15 <felipeantoniodesouzaesilva@gmail.com>
2025-02-18 10:46:29 -0300

    Colisões e Movimento Terminados

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,40 +7,35 @@ public class PlayerBehaviour : MonoBehaviour
 {
     public UnityEvent Reset;
     public Transform player;
-
-    public Vector3 playerPos;
+    public Rigidbody2D physicsPlayer;
 
     public float flySpd = 0.01f;
     private float _newHeight;
     void Start()
     {
         player = GetComponent<Transform>();
+        physicsPlayer = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
+        if (Input.touchCount > 0)
         Jump();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Floor") || collision.gameObject.CompareTag("Pipe"))
         {
-            //Reset.Invoke();
+            Reset.Invoke();
         }
     }
     void Jump()
     {
 
-        if (Input.touchCount > 0)
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
         {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
-            {
-                _newHeight = player.position.y + flySpd;
-
-                playerPos = new Vector3(player.position.x, _newHeight, 0);
-
-                player.position = Vector3.Lerp(player.position, playerPos, flySpd * Time.deltaTime);
-            }
+            physicsPlayer.velocity = Vector2.up * flySpd;
         }
     }
 }
