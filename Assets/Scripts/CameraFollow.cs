ac495fc1943a0f94bef6238cdefeaff7f551f237
Fonazo15 <felipeantoniodesouzaesilva@gmail.com>
2025-04-02 09:36:32 -0300

    Labirinto

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public PlayerBehaviour player;
+    // public PlayerBehaviour player;
+    public Transform playerTransform;
+    public Vector3 offset;
+    public float spd = 5f;
     void Update()
     {
-        MoveCam();
+        //MoveCam();
+        Vector3 targetPosition = playerTransform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, spd * Time.deltaTime);
     }
+    /*
     void MoveCam()
     {
         transform.position = new Vector3(Mathf.Lerp(transform.position.x, player.transform.position.x, player.spd * Time.deltaTime), transform.position.y, Mathf.Lerp(transform.position.z, player.transform.position.z - 18, player.spd * Time.deltaTime));
     }
+    */
 }
--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float spd;
+    public float switchTime = 2f;
+    public int direction = 1;
+
+    private float _time;
+    //public Vector3[] possibleMoves;
     void Start()
     {
-        
+        _time = switchTime;
+        //StartCoroutine(EnemyMove());
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _time -= Time.deltaTime;
+
+        if (_time <= 0 )
+        {
+            direction *= -1;
+            _time = switchTime;
+        }
+        transform.position += Vector3.right * direction * spd * Time.deltaTime;
     }
+    /*
+    void RandomMove()
+    {
+        transform.position = possibleMoves[Random.Range(0, 3)];  //new Vector3 (Mathf.LerpUnclamped(transform.position.x, possibleMoves[Random.Range(0, 3)].x, spd), transform.position.y, Mathf.LerpUnclamped(transform.position.z, possibleMoves[Random.Range(0, 3)].z, spd))
+    }
+    IEnumerator EnemyMove()
+    {
+        RandomMove();
+        yield return new WaitForSeconds(Random.Range(1f, 3f));
+        StartCoroutine(EnemyMove());
+    }
+    */
 }
