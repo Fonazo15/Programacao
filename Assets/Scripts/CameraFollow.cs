9d14e9b2206e7f088c620e798d504790e0831adc
Fonazo15 <felipeantoniodesouzaesilva@gmail.com>
2025-04-09 09:35:50 -0300

    Challange Quick Delivery Start

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 offset;
+    public float spd = 5f;
+    void Update()
+    {
+        Vector3 targetPosition = playerTransform.position + offset;
+        transform.position = targetPosition;
+        LookAtMouse();
+    }
+    void LookAtMouse()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 targetRotation = new Vector3(mouse.y * -1, mouse.x * -1);
+        if (targetRotation.y <= 90 || targetRotation.y >= -90)
+            transform.rotation = Quaternion.Euler(targetRotation);
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float spd = 5f;
+    public float jump = 2f;
+    public float gravity = -10f;
+
+    private CharacterController _controller;
+    private Vector3 _velocity;
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        HandleGround();
+        HandleVision();
+        HandleMovement();
+        HandleJump();
+        ApplyGravity();
+    }
+    void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, _velocity.y, z);
+        _controller.Move(move * spd * Time.deltaTime);
+    }
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && _controller.isGrounded)
+        {
+            _velocity.y = jump;
+        }
+    }
+    void ApplyGravity()
+    {
+        _velocity.y += gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+    void HandleGround()
+    {
+        if (_controller.isGrounded && _velocity.y < 0)
+            _velocity.y = -(jump * 0.75f * Time.deltaTime);
+    }
+    void HandleVision()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 targetRotation = new Vector3(mouse.y * -1, 0);
+        transform.rotation = Quaternion.Euler(targetRotation);
+    }
+}
